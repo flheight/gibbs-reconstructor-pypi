ff5b0a8a05268ab794645228fa0d5750c86e13c4
flheight <75139846+flheight@users.noreply.github.com>
2024-09-28 11:05:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gibbs-reconstructor",
-    version="1.0.0",
+    version="1.0.1",
     author="Félix Laplante",
     author_email="flheight0@gmail.com",
     description="Gibbs Reconstruction for linear data reconstruction",
